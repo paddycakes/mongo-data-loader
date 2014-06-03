83f45eec14fe18551c95d78f4f71ff59f80eddea
Patrick Gallagher <paddy.gallagher@gmail.com>
2014-06-03 22:39:06 +0100

    Adding Scala Casbah Mongo driver to dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,11 +4,15 @@ object Version {
   val scala        = "2.11.0"
   val scalaParsers = "1.0.1"
   val scalaTest    = "2.1.4"
+  val mongoCasbah = "2.7.2"
+  // val slf4j = "1.6.4"
 }
 
 object Library {
   val scalaParsers = "org.scala-lang.modules" %% "scala-parser-combinators" % Version.scalaParsers
   val scalaTest    = "org.scalatest"          %% "scalatest"                % Version.scalaTest
+  val mongoCasbah    = "org.mongodb"          %% "casbah"                % Version.mongoCasbah
+  // val slf4j    = "org.slf4j"          %% "slf4j-simple"                % Version.slf4j
 }
 
 object Dependencies {
@@ -17,6 +21,8 @@ object Dependencies {
 
   val mongoDataLoader = List(
     scalaParsers,
-    scalaTest % "test"
+    scalaTest % "test",
+    mongoCasbah //,
+//    slf4j
   )
 }
