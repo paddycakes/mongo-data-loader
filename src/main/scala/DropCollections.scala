260817288faaa4e6586e511c2aa0a788fb3c3ec4
Patrick Gallagher <paddy.gallagher@gmail.com>
2014-06-03 23:46:19 +0100

    Script will drop all zook domain collections from datastore

--- src/main/scala/DropCollections.scala
+++ src/main/scala/DropCollections.scala
@@ -1,11 +1,30 @@
 import com.mongodb.casbah.Imports._
 
+/*
+Zook Collections
+----------------
+bookings
+businesses
+employees
+payments
+services
+subscriptions
+tokens
+users
+ */
+
+// TODO: Should this be an object with a main method, ie. not extend App
 object DropCollections extends App {
 
   val mongoClient = MongoClient("localhost", 27017)
 
   val db = mongoClient("zook")
 
-  db.collectionNames.foreach(collection => println(collection))
+  val zookCollections = db.collectionNames.filter(name => name != "system.indexes")
 
+  println("Zook collections to be dropped:")
+  zookCollections.foreach(println(_))
+
+  zookCollections.foreach(db(_).drop())
+  println("Zook collections dropped")
 }
